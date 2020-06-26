e09fb158238d78b2f45601e9f0dd68246d355d2f
Anya Gessesse <anyagess@fb.com>
2020-06-26 09:19:48 -0700

    can view details in separate activity

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,8 +14,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.MovieDetailsActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.io.File;
 import java.util.List;
@@ -52,7 +56,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvTitle;
         TextView tvOverview;
@@ -63,6 +67,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+
+            itemView.setOnClickListener(this);
         }
 
         public void bind(Movie movie) {
@@ -84,6 +90,24 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             }
 
             Glide.with(context).load(imageUrl).placeholder(placeHolder).into(ivPoster);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //get item position and check if valid
+            int pos = getAdapterPosition();
+            if(pos != RecyclerView.NO_POSITION){
+                //get movie at this position
+                Movie movie = movies.get(pos);
+                //create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                //serialize using parceler, use short name as key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                //show the activity
+                context.startActivity(intent);
+            };
+
+
         }
     }
 }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -5,22 +5,30 @@ import com.example.flixster.R;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+    Double voteAverage;
+
+    //no-arg, empty constructor required for Parceler
+    public Movie(){
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getDouble("vote_average");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -46,5 +54,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
